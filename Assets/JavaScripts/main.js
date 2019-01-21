cea916c8546598060eaeef072fa286a5fdbe150a
Boris Schauer <me@bschauer.de>
2019-01-21 12:11:24 +0100

    [TASK] Add some helpers to js

--- Assets/JavaScripts/main.js
+++ Assets/JavaScripts/main.js
@@ -1,5 +1,6 @@
 // Import jQuery global
 import $ from 'jquery';
+
 window.jQuery = $;
 window.$ = $;
 
@@ -8,7 +9,15 @@ import 'bootstrap'
 
 // Example for modules
 import {sayhello} from "./modules/sayhello";
+import {srcsethelper} from "./modules/srcset-helper";
+import {devLog} from "./modules/debug";
 
-$(function(){
+// DEBUG
+const devDomain = 'localhost';
+export {devDomain};
+
+$(function () {
     sayhello('Bo');
+    devLog('Console Log');
+    srcsethelper();
 });
